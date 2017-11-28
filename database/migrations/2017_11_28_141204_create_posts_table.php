997d12e91d6db7e6896d082ea78aa5cf022a2beb
Alexandr Shevchenko <alexandr@comet.by>
2017-11-28 17:16:20 +0300

    Have cleaned laravel and added migrations

---
+++ database/migrations/2017_11_28_141204_create_posts_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('header');
+            $table->text('content');
+            $table->integer('category_id')->unsigned();
+            $table->timestamps();
+
+            $table->foreign('category_id')->references('id')->on('categories');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
