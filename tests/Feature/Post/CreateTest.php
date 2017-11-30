63d32bec36a971bd18cdbc05616e7711186128c6
Alexandr Shevchenko <alexandr@comet.by>
2017-11-30 15:48:22 +0300

    Save point

---
+++ tests/Feature/Post/CreateTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Tests\Feature\Post;
+
+use App\Helpers\TestHelper;
+use Tests\TestCase;
+
+/**
+ * Class CreateTest
+ * @package Tests\Feature\Post
+ */
+class CreateTest extends TestCase
+{
+    use TestHelper;
+
+
+    public function positive()
+    {
+        //Arrange
+
+    }
+}
--- tests/Feature/Post/GetTest.php
+++ tests/Feature/Post/GetTest.php
@@ -19,10 +19,12 @@ class GetTest extends TestCase
 
     /**
      * @test
+     * @dataProvider userRolesDataProvider
      */
-    public function positive()
+    public function testWithUserRoles($creds, $success)
     {
         //Arrange
+        $token = is_null($creds) ? 'some.shit' : $this->doLogin($creds);
 
         /** @var Post $expectedPost */
         $expectedPost = AppMake::Post()
@@ -30,32 +32,70 @@ class GetTest extends TestCase
             ->with('category')
             ->find(3);
 
-        $token = $this->doLogin(\UserSeeder::USER_SEEDS['SUBSCRIBER']);
-
         //Act
         $response = $this->get(
             route("post::get", ['id' => $expectedPost->id]),
             $this->authHeaderForToken($token));
 
         //Assert
-        $response->assertStatus(Response::HTTP_OK);
+        if (!$success) {
+            $response->assertStatus(Response::HTTP_UNAUTHORIZED);
+        } else {
+            $response->assertStatus(Response::HTTP_OK);
 
-        $response->assertJsonStructure([
-            'header',
-            'content',
-            'category' => [
-                'id',
-                'name',
-            ]
-        ]);
+            $response->assertJsonStructure([
+                'header',
+                'content',
+                'category' => [
+                    'id',
+                    'name',
+                ]
+            ]);
 
-        $response->assertJson([
-            'header' => $expectedPost->header,
-            'content' => $expectedPost->content,
-            'category' => [
-                'id' => $expectedPost->category->id,
-                'name' => $expectedPost->category->name,
-            ]
-        ]);
+            $response->assertJson([
+                'header' => $expectedPost->header,
+                'content' => $expectedPost->content,
+                'category' => [
+                    'id' => $expectedPost->category->id,
+                    'name' => $expectedPost->category->name,
+                ]
+            ]);
+        }
+    }
+
+    /**
+     * @test
+     * @dataProvider userRolesDataProvider
+     */
+    public function testWrongId($creds, $authSuccess)
+    {
+        //Arrange
+        $token = is_null($creds) ? 'some.shit' : $this->doLogin($creds);
+
+        $wrongPostId = AppMake::Post()
+                ->newQuery()
+                ->max('id') * 2;
+
+        //Act
+        $response = $this->get(
+            route("post::get", ['id' => $wrongPostId]),
+            $this->authHeaderForToken($token));
+
+        //Assert
+        if (!$authSuccess) {
+            $response->assertStatus(Response::HTTP_UNAUTHORIZED);
+        } else {
+            $response->assertStatus(Response::HTTP_NOT_FOUND);
+        }
+    }
+
+    public function userRolesDataProvider()
+    {
+        return [
+            [null, false],
+            [\UserSeeder::USER_SEEDS['SUBSCRIBER'], true],
+            [\UserSeeder::USER_SEEDS['EDITOR'], true],
+            [\UserSeeder::USER_SEEDS['ADMIN'], true],
+        ];
     }
 }
